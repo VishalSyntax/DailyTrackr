2ae2b519fbf4442ca7c5c5e734743da242b4bdf1
VishalSyntax <thatvishal007@gmail.com>
2026-04-20 23:58:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-import 'screens/home_screen.dart';
-import 'services/local_storage_service.dart';
+import 'services/hive_storage_service.dart';
+import 'viewmodels/habit_view_model.dart';
+import 'views/app_shell.dart';
 
-void main() {
-  runApp(const DailyTrackrApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  final box = await Hive.openBox<dynamic>(HiveStorageService.boxName);
+  runApp(
+    DailyTrackrApp(
+      viewModel: HabitViewModel(
+        storageService: HiveStorageService(box),
+      ),
+    ),
+  );
 }
 
-class DailyTrackrApp extends StatelessWidget {
-  const DailyTrackrApp({super.key, this.storageService});
+class DailyTrackrApp extends StatefulWidget {
+  const DailyTrackrApp({super.key, required this.viewModel});
 
-  final LocalStorageService? storageService;
+  final HabitViewModel viewModel;
+
+  @override
+  State<DailyTrackrApp> createState() => _DailyTrackrAppState();
+}
+
+class _DailyTrackrAppState extends State<DailyTrackrApp> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.initialize();
+  }
+
+  @override
+  void dispose() {
+    widget.viewModel.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Habit Tracker',
       theme: ThemeData(colorSchemeSeed: Colors.green, useMaterial3: true),
-      home: HomeScreen(
-        storageService: storageService ?? LocalStorageService(),
-      ),
+      home: AppShell(viewModel: widget.viewModel),
     );
   }
 }
